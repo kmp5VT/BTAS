9cf737707d2f71e07969196b6bb90209fbb656c0
kmp5VT <karlpierce@Karls-MacBook-Pro.local>
2018-07-26 10:06:59 -0400

    Change to more reliable random number generator

--- btas/generic/cp_als.h
+++ btas/generic/cp_als.h
@@ -517,8 +517,9 @@ namespace btas {
           A[i] = lambda;
         }
 
-        srand(3);
+        //srand(3);
 
+        std::mt19937 generator(3);
         // Fill the remaining columns in the set of factor matrices with dimension < SVD_rank with random numbers
         for(auto& i: modes_w_dim_LT_svd){
           int R = tensor_ref.extent(i);
@@ -527,7 +528,8 @@ namespace btas {
           auto upper_bound = {R, SVD_rank};
           auto view = make_view(A[i].range().slice(lower_bound, upper_bound), A[i].storage());
           for(auto iter = view.begin(); iter != view.end(); ++iter){
-            *(iter) = rand() % num_elements;
+            //*(iter) = rand() % num_elements;
+            *(iter) = generator() % num_elements;
           }
         }
 
